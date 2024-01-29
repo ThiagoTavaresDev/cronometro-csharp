05e123ef6fdbe2bb8e2ac1028bd178a225c46153
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-01-29 11:29:22 -0300

    cronometro-csharp

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Cronometro{
+
+    class Program{
+
+        static void Main(string[] args){
+            Menu();
+        }
+        static void StartTimer(){
+            Console.WriteLine("Até quanto deseja cronometrar? --- Digite o tempo em segundos");
+            int tempo = int.Parse(Console.ReadLine());
+            int tempoAtual = 0;
+            while(tempo > tempoAtual){
+                tempoAtual++;
+                Console.Clear();
+                Console.WriteLine(tempoAtual);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Menu(){
+            Console.WriteLine("Bem vindo ao menu do cronômetro");
+            Console.WriteLine("Digite uma opção:");
+            Console.WriteLine("1 - Iniciar o cronômetro");
+            Console.WriteLine("2 - Sair");
+            if(Console.ReadLine() == "1"){
+                StartTimer();
+            }
+            else{
+                Environment.Exit(0);
+            }
+        }
+
+    }
+
+
+}
